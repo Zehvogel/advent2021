10e7e7a2318f5b033ee89c50a4f40168d42aa557
Zehvogel <lreichen@physnet.uni-hamburg.de>
2021-12-01 10:35:18 +0100

    first commit

---
+++ day1/solution.py
@@ -0,0 +1,28 @@
+from more_itertools import pairwise, windowed
+
+
+def part1(input):
+    res = 0
+    for a, b in pairwise(input):
+        if a < b:
+            res += 1
+    return res
+
+def tmsums(input):
+    for a in windowed(input, 3):
+        yield sum(a)
+
+def part2(input):
+    sums = tmsums(input)
+    return part1(sums)
+
+def main():
+    with open("day1/input.txt") as f:
+        f_int = (int(a) for a in f)
+        print(f"solution for part 1 is: {part1(f_int)}")
+        f.seek(0)
+        f_int = (int(a) for a in f)
+        print(f"solution for part 2 is: {part2(f_int)}")
+
+if __name__ == "__main__":
+    main()
