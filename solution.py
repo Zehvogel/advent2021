0b1106fd56a69a9cc3b62de52b23ebb6523db6a7
Zehvogel <lreichen@physnet.uni-hamburg.de>
2021-12-12 12:50:43 +0100

    renamed single digit days for correct ordering

---
+++ solution.py
@@ -0,0 +1,22 @@
+import time
+
+
+def part1(input):
+    return ""
+
+def part2(input):
+    return ""
+
+def main():
+    with open("day11/input.txt") as f:
+        input =  f.readlines()
+        t0 = time.perf_counter_ns()
+        print(f"solution for part 1 is: {part1(input)}")
+        t1 = time.perf_counter_ns()
+        print(f"part 1 took {t1-t0} ns")
+        print(f"solution for part 2 is: {part2(input)}")
+        t2 = time.perf_counter_ns()
+        print(f"part 2 took {t2-t1} ns")
+
+if __name__ == "__main__":
+    main()
